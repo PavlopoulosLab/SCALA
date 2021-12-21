406b8e912fec64a13fb1faa01157d7da3c55580e
Fotis Baltoumas <baltoumas@fleming.gr>
2021-12-21 15:11:31 +0200

    Add files via upload

--- install_datasets.sh
+++ install_datasets.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-wget -P scenic_helper_files https://zenodo.org/record/3260758/files/ligand_target_matrix.rds
-wget -P scenic_helper_files https://zenodo.org/record/3260758/files/lr_network.rds
-wget -P scenic_helper_files https://zenodo.org/record/3260758/files/weighted_networks.rds
+wget -P . https://zenodo.org/record/3260758/files/ligand_target_matrix.rds
+wget -P . https://zenodo.org/record/3260758/files/lr_network.rds
+wget -P . https://zenodo.org/record/3260758/files/weighted_networks.rds
 wget -P scenic_helper_files https://resources.aertslab.org/cistarget/databases/mus_musculus/mm9/refseq_r45/mc9nr/gene_based/mm9-500bp-upstream-10species.mc9nr.feather
 wget -P scenic_helper_files https://resources.aertslab.org/cistarget/databases/mus_musculus/mm9/refseq_r45/mc9nr/gene_based/mm9-tss-centered-10kb-10species.mc9nr.feather
 wget -P scenic_helper_files https://resources.aertslab.org/cistarget/databases/mus_musculus/mm10/refseq_r80/mc9nr/gene_based/mm10__refseq-r80__10kb_up_and_down_tss.mc9nr.feather
@@ -19,6 +19,7 @@ wget -P scenic_helper_files https://raw.githubusercontent.com/aertslab/pySCENIC/
 
 wget -P . http://bib.fleming.gr:8084/SCANNER/example_files/example_form_files.tgz
 tar xvf example_form_files.tgz
+rm example_form_files.tgz
 
 
 wget -P www/example_files/ http://bib.fleming.gr:8084/SCANNER/example_files/matrix.mtx.gz
